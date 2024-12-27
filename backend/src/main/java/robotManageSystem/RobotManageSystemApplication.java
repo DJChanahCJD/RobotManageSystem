5ed2061087407f3164ae62c0694c758acae89c8e
浪小矢 <djchan411130@gmail.com>
2024-12-27 14:49:46 +0800

    配置好pom.xml

--- backend/src/main/java/robotManageSystem/RobotManageSystemApplication.java
+++ backend/src/main/java/robotManageSystem/RobotManageSystemApplication.java
@@ -2,12 +2,18 @@ package robotManageSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 启动类，需要扫描华为 SDK 包和用户项目目录包
+ *
+ * @since 2024-04-10
+ */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.huawei.innovation", "robotManageSystem"})
 public class RobotManageSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RobotManageSystemApplication.class, args);
     }
-
 }
