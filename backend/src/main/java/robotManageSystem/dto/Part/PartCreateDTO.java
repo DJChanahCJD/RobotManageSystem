6d320f064fc7705c5f7ce905731369b4cf56c8fb
御坂美琴20001 <168548529+yubanmeiqin20001@users.noreply.github.com>
2025-01-01 23:40:56 +0800

    Add files via upload

---
+++ backend/src/main/java/robotManageSystem/dto/Part/PartCreateDTO.java
@@ -0,0 +1,29 @@
+package robotManageSystem.dto.Part;
+
+import com.huawei.innovation.rdm.xdm.dto.relation.EXADefinitionLinkViewDTO;
+
+import javax.validation.Valid;
+import java.util.List;
+// PartCreateDTO.java
+public class PartCreateDTO extends com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.@Valid PartCreateDTO {
+    private Long classificationId;
+    private List<EXADefinitionLinkViewDTO> attributes;
+    // 其他字段和方法
+
+    public Long getClassificationId() {
+        return classificationId;
+    }
+
+    public void setClassificationId(Long classificationId) {
+        this.classificationId = classificationId;
+    }
+
+    public List<EXADefinitionLinkViewDTO> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(List<EXADefinitionLinkViewDTO> attributes) {
+        this.attributes = attributes;
+    }
+}
+
